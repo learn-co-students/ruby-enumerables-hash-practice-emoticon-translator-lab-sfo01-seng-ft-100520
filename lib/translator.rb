e31de94903dca9fa68557a289625c0bfc8ccd2f2
Alexa Gamil <alexis_gamil99@yahoo.com>
2020-09-10 21:38:08 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,46 +1,45 @@
 require 'pry'
 require 'yaml'
 
-def load_library(file)
-  emoticons = YAML.load_file(file)
- 
-  emoticons.each_with_object ({}) do |(key, value), new_hash|
-    value.each do 
+def load_library (file)
+emoticons = YAML.load_file(file)
+emoticons.each_with_object({}) do |(expression, faces), final_hash|
+  faces.each do ||
     
-      if !new_hash[key]
-      new_hash[key] = {"english": value[0],
-                       "japanese": value[1]}
-      end
+    if !final_hash[expression]
+      final_hash[expression] = { :english => faces[0],
+                                 :japanese => faces[1] }
     end
-    new_hash
   end
+  final_hash
+end
 end
 
 
-def get_japanese_emoticon(file, emoticon)
-  load_library(file).each do |english_name, value|
-    value.each do |version, face|
-    
-      if face == emoticon
-        return load_library(file)[english_name][:japanese]
+def get_english_meaning (file, emoticon)
+  emoticons = load_library(file)
+  
+  emoticons.each do |expression, versions|
+    versions.each do |english, japanese|
+      if emoticon == japanese
+        return expression
       end
       
-    end  
+    end
   end
   return "Sorry, that emoticon was not found"
 end
 
-
-
-def get_english_meaning(file, emoticon)
-  load_library(file).each do |english_name, value| 
-    value.each do |version, face|
+def get_japanese_emoticon (file, emoticon)
+  emoticons = load_library(file)
+  
+  emoticons.each do |expression, versions|
+    versions.each do |english, face|
       if emoticon == face
-        return english_name
+        return versions[:japanese]
       end
-      
     end
-  end
-    return "Sorry, that emoticon was not found"
+  end   
+  return "Sorry, that emoticon was not found"
 end
-
+  
